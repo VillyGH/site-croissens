9a97d15c60bb69591d7964649a761315716c1a0f
William Blanchet Lafrenière <willclash@outlook.com>
2023-01-04 23:09:46 -0500

    fix vue3

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,21 +4,29 @@
       <b-form @submit.prevent="register">
         <h1>Créer un compte</h1>
         <label>Courriel</label>
-        <b-form-input required v-model="email" type="text" />
-        <label>Nom d'utilisateur</label>
-        <b-form-input required v-model="name" type="text" />
+        <b-form-input required v-model="email" placeholder="Adresse courriel" type="text" />
 
         <label>Mot de passe</label>
-        <b-form-input required v-model="password" type="password" />
+        <b-form-input required v-model="password" placeholder="Mot de passe" type="password" />
         <b-button type="submit" variant="primary" class="mt-4"
           >Créer le compte</b-button
         >
       </b-form>
+      <b-button @click="signInWithGoogle">Se connecter avec Google</b-button>
       <div v-if="authServiceError"></div>
     </b-container>
   </div>
 </template>
 <script>
+import { getAuth, createUserWithEmailAndPassword } from "@firebase/util";
+import { ref } from "vue";
+const email = ref("");
+const password = ref("");
+
+const register = () => {
+  createUserWithEmailAndPassword(getAuth(), email.value, password.value);
+}
+
 export default {
   created() {
     this.$store.commit("authentication/clearError");
