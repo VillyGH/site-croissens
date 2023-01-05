0bebd4e72b55d5798b6efd20a7785666c63cb046
William Blanchet Lafrenière <willclash@outlook.com>
2023-01-04 21:55:16 -0500

    vue config

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,20 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = {
   chainWebpack: (config) => {
-    config.resolve.alias.set('vue', '@vue/compat')
+    config.resolve.alias.set("vue", "@vue/compat");
 
     config.module
-      .rule('vue')
-      .use('vue-loader')
+      .rule("vue")
+      .use("vue-loader")
       .tap((options) => {
         return {
           ...options,
           compilerOptions: {
             compatConfig: {
-              MODE: 2
-            }
-          }
-        }
-      })
-  }
-}
+              MODE: 2,
+            },
+          },
+        };
+      });
+  },
+};
