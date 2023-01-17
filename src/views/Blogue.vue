0833b9c10cac98fb7df0b8005f96833bccf7a82f
William Blanchet Lafrenière <willclash@outlook.com>
2023-01-16 21:57:31 -0500

    comments working

--- src/views/Blogue.vue
+++ src/views/Blogue.vue
@@ -24,25 +24,39 @@
         <div class="mt-4 text-danger">{{ errorMessage }}</div>
         <h3 v-if="comments.length != 0">{{ comments.length }} Commentaires</h3>
         <h3 v-else>Aucun Commentaire</h3>
-        <div class="mb-4" v-bind:key="comment" v-for="comment in comments">
-          <div></div>
+        <div class="mb-6" v-bind:key="comment" v-for="comment in comments">
           <div>{{ comment.owner }}</div>
           <div>
+            <span v-if="comment.modified">Modifié </span>
             {{ moment(comment.date).fromNow() }}
           </div>
-          <div class="mt-4">
-            {{ comment.text }}
-            <b-icon-pencil-square class="ml-5"
-              ><button @click="editMode(comment)"></button
-            ></b-icon-pencil-square>
-            <b-button
-              @click="createComment"
-              variant="primary"
-              class="mt-4"
-              id="confirmEditBtn"
-              hidden
-              >Confirmer</b-button
-            >
+          <div class="mb-4">
+            <textarea
+              v-model="comment.text"
+              class="input-message mt-3"
+              id="comment"
+              rows="1"
+              cols="45"
+              required
+              v-bind:disabled="editedCommentId != comment.id"
+            ></textarea>
+            <a
+              v-if="isLoggedIn && comment.owner == username"
+              @click="changeEditMode(comment.id)"
+              class="ml-4"
+              ><b-icon-pencil-square></b-icon-pencil-square>
+            </a>
+            <div>
+              <b-button
+                @click="editComment(comment)"
+                variant="primary"
+                class="mt-2"
+                id="confirmEditBtn"
+                v-bind:hidden="editedCommentId != comment.id"
+                >Confirmer</b-button
+              >
+            </div>
+            <div class="mt-4 text-danger">{{ editErrorMessage }}</div>
           </div>
         </div>
       </b-col>
@@ -55,6 +69,7 @@ import { onMounted, ref } from "vue";
 import { auth, db } from "@/firebase/firebaseInit";
 import {
   addDoc,
+  updateDoc,
   collection,
   doc,
   getDoc,
@@ -68,7 +83,10 @@ const createdComment = ref("");
 const isLoggedIn = ref(false);
 const errorMessage = ref("");
 const successMessage = ref("");
+const editErrorMessage = ref("");
 const comments = ref([]);
+const username = ref("");
+const editedCommentId = ref("");
 onMounted(async () => {
   moment.locale("fr");
   onAuthStateChanged(auth, (user) => {
@@ -79,14 +97,14 @@ onMounted(async () => {
     }
   });
   await loadComments();
+  username.value = await getCurrentUsername();
 });
 
 const createComment = async () => {
   if (createdComment.value.length >= 3 && createdComment.value.length <= 450) {
-    const username = await getCurrentUsername();
     const commentData = {
-      date: moment().format("MMMM Do YYYY, h:mm:ss"),
-      owner: username,
+      date: moment().format("MMMM Do YYYY, HH:mm:ss"),
+      owner: username.value,
       text: createdComment.value,
     };
     await addDoc(collection(db, `comments`), commentData);
@@ -99,9 +117,28 @@ const createComment = async () => {
   }
 };
 
-const editMode = async (comment) => {
-  document.getElementById("confirmEditBtn").hidden = false;
-  console.log("t");
+const editComment = async (comment) => {
+  if (comment.text.length >= 3 && comment.text.length <= 450) {
+    const commentData = {
+      date: moment().format("MMMM Do YYYY, HH:mm:ss"),
+      text: comment.text,
+      modified: true,
+    };
+    await updateDoc(doc(db, "comments", comment.id), commentData);
+    editErrorMessage.value = "";
+    window.location.reload();
+  } else {
+    editErrorMessage.value =
+      "Le commentaire doit avoir entre 3 et 450 caractères";
+  }
+};
+
+const changeEditMode = async (id) => {
+  if (editedCommentId.value == "") {
+    editedCommentId.value = id;
+  } else {
+    editedCommentId.value = "";
+  }
 };
 
 const getCurrentUsername = async () => {
@@ -114,7 +151,9 @@ const getCurrentUsername = async () => {
 const loadComments = async () => {
   const querySnapshot = await getDocs(query(collection(db, "comments")));
   querySnapshot.forEach((doc) => {
-    comments.value.push(doc.data());
+    const newDoc = doc.data();
+    newDoc.id = doc.id;
+    comments.value.push(newDoc);
   });
 };
 </script>
